da41134c5d31897de4ede01e0653e376bda0cf2c
Pokiza Sdv <pokizasdv@gmail.com>
2023-06-05 17:36:40 -0400

    Create Assignment-5

---
+++ assignments/assignment-5/node.js
@@ -0,0 +1,14 @@
+var name = "John";
+var age = "30";
+var city = "New York";
+var profession = "software engineer";
+var hobby1 = "reading books";
+var hobby2 = "playing the guitar";
+var favoriteFood = "pizza";
+var favoriteColor = "blue";
+var destination = "Tokyo";
+var favoriteSeason = "autumn";
+
+var result = `Hello, my name is ${name} and I am ${age} years old. I live in ${city}, where I work as a ${profession}. In my free time, I enjoy ${hobby1} and ${hobby2}. My favorite food is ${favoriteFood} and my favorite color is ${favoriteColor}. My dream is to one day visit ${destination}. My favorite season of the year is ${favoriteSeason} because of the beautiful changing colors of the leaves`;
+
+console.log(result);
