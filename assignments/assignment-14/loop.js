0b5995dd280979aca906fb57434295d3b0a20fca
Pokiza Sdv <pokizasdv@gmail.com>
2023-06-16 17:06:56 -0400

    Create Assignment-14

---
+++ assignments/assignment-14/loop.js
@@ -0,0 +1,49 @@
+// 1 Function to print numbers from 11 to 99 in ascending order:
+
+function NumbersBetween11and99() {
+    for (var num = 11; num <= 99; num++) {
+        console.log(num);
+    }
+}
+
+// 2. Function to print numbers between 6 and 76 that are divisible by 7:
+
+function DivisibleBy7() {
+    for (var num = 6; num <= 76; num++) {
+        if (num % 7 === 0) {
+            console.log(num);
+        }
+    }
+}
+
+// 3. Function to print numbers between 1 and 99 that are divisible by both 3 and 5:
+
+function DivisibleBy3And5() {
+    for (var num = 1; num <= 99; num++) {
+        if (num % 3 === 0 && num % 5 === 0) {
+            console.log(num);
+        }
+    }
+}
+
+// 4. Function to calculate and print the squares of numbers between 20 and 40:
+
+function SquaresBetween20and40() {
+    for (var num = 20; num <= 40; num++) {
+        var square = num * num;
+        console.log(square);
+    }
+}
+
+// 5. Function that prints all numbers between 1 and 99 that are divisible by either 3 or 5, but not both at the same time.
+
+function DivisibleBy3Or5() {
+    for (var num = 1; num <= 99; num++) {
+        if (
+            (num % 3 === 0 && num % 5 !== 0) ||
+            (num % 3 !== 0 && num % 5 === 0)
+        ) {
+            console.log(num);
+        }
+    }
+}
