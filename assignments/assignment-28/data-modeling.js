276ef60de07ae6fdb02c71f939eaa7b2d07b404c
Pokiza Sdv <pokizasdv@gmail.com>
2023-07-30 23:41:21 -0400

    Finish Assignment-27

---
+++ assignments/assignment-28/data-modeling.js
@@ -0,0 +1,99 @@
+let book = {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    yearPublished: 1925
+};
+let book = {
+    title: "The Great Gatsby",
+    author: "F. Scott Fitzgerald",
+    yearPublished: 1925,
+    genres: ["Fiction", "Classic"],
+    ratings: [
+        { user: "user1", score: 4 },
+        { user: "user2", score: 5 },
+        { user: "user3", score: 3 }
+    ]
+};
+let library = {
+    books: [
+        {
+            title: "The Great Gatsby",
+            author: "F. Scott Fitzgerald",
+            yearPublished: 1925,
+            genres: ["Fiction", "Classic"],
+            ratings: [
+                { user: "user1", score: 4 },
+                { user: "user2", score: 5 },
+                { user: "user3", score: 3 }
+            ]
+        },
+        {
+            title: "To Kill a Mockingbird",
+            author: "Harper Lee",
+            yearPublished: 1960,
+            genres: ["Fiction", "Classic"],
+            ratings: [
+                { user: "user2", score: 5 },
+                { user: "user4", score: 4 },
+                { user: "user5", score: 4 }
+            ]
+        },
+        {
+            title: "1984",
+            author: "George Orwell",
+            yearPublished: 1949,
+            genres: ["Fiction", "Dystopian"],
+            ratings: [
+                { user: "user1", score: 5 },
+                { user: "user3", score: 4 },
+                { user: "user6", score: 5 }
+            ]
+        }
+    ]
+};
+
+let library = {
+    books: [
+        {
+            title: "The Great Gatsby",
+            author: "F. Scott Fitzgerald",
+            yearPublished: 1925,
+            genres: ["Fiction", "Classic"],
+            ratings: [
+                { user: "user1", score: 4 },
+                { user: "user2", score: 5 },
+                { user: "user3", score: 3 }
+            ]
+        },
+        {
+            title: "To Kill a Mockingbird",
+            author: "Harper Lee",
+            yearPublished: 1960,
+            genres: ["Fiction", "Classic"],
+            ratings: [
+                { user: "user2", score: 5 },
+                { user: "user4", score: 4 },
+                { user: "user5", score: 4 }
+            ]
+        },
+        {
+            title: "1984",
+            author: "George Orwell",
+            yearPublished: 1949,
+            genres: ["Fiction", "Dystopian"],
+            ratings: [
+                { user: "user1", score: 5 },
+                { user: "user3", score: 4 },
+                { user: "user6", score: 5 }
+            ]
+        }
+    ],
+    users: [
+        { id: "user1", name: "John Doe" },
+        { id: "user2", name: "Jane Smith" },
+        { id: "user3", name: "Alice Johnson" },
+        { id: "user4", name: "Bob Anderson" },
+        { id: "user5", name: "Emily Davis" },
+        { id: "user6", name: "Michael Wilson" }
+    ]
+};
