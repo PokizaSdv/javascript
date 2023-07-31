513677db888928dffebe61fdad32c070d84d031d
Pokiza Sdv <pokizasdv@gmail.com>
2023-07-31 00:13:33 -0400

    Finish Assignment-28

---
+++ assignments/assignment-28/data-modeling.js
@@ -0,0 +1,71 @@
+const book1 = {
+    title: "Book One",
+    author: "Author One",
+    yearPublished: 2020,
+    genres: ["Fiction", "Mystery"],
+    ratings: [
+        { user: "user1", score: 4.5 },
+        { user: "user2", score: 3.8 }
+    ]
+};
+const book2 = {
+    title: "Book Two",
+    author: "Author Two",
+    yearPublished: 2018,
+    genres: ["Science Fiction"],
+    ratings: [
+        { user: "user1", score: 4.2 },
+        { user: "user3", score: 3.9 }
+    ]
+};
+
+const book3 = {
+    title: "Book Three",
+    author: "Author Three",
+    yearPublished: 2015,
+    genres: ["Romance", "Drama"],
+    ratings: [
+        { user: "user2", score: 4.0 },
+        { user: "user4", score: 4.5 }
+    ]
+};
+
+const users = [
+    { id: "user1", name: "John" },
+    { id: "user2", name: "Emily" },
+    { id: "user3", name: "Michael" },
+    { id: "user4", name: "Sophia" }
+];
+
+const books = [
+    {
+        title: "Book One",
+        author: "Author One",
+        yearPublished: 2020,
+        genres: ["Fiction", "Mystery"],
+        ratings: [
+            { user: "user1", score: 4.5 },
+            { user: "user2", score: 3.8 }
+        ]
+    },
+    {
+        title: "Book Two",
+        author: "Author Two",
+        yearPublished: 2018,
+        genres: ["Science Fiction"],
+        ratings: [
+            { user: "user1", score: 4.2 },
+            { user: "user3", score: 3.9 }
+        ]
+    },
+    {
+        title: "Book Three",
+        author: "Author Three",
+        yearPublished: 2015,
+        genres: ["Romance", "Drama"],
+        ratings: [
+            { user: "user2", score: 4.0 },
+            { user: "user4", score: 4.5 }
+        ]
+    }
+];
